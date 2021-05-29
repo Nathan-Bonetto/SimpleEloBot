b22e832daa61c6f3389ba0cfd778c421a37a186a
bonet <nathan.rbonetto@gmail.com>
2021-05-28 20:58:51 -0700

    Added the the elo calculator, sorted channel permissions, created more channels, also changed it so that a mod will have to register/resign a player

--- db.py
+++ db.py
@@ -1,4 +1,3 @@
-import os
 import mysql.connector
 
 DB = ""
@@ -42,6 +41,54 @@ def removeMember(name, db):
 
 def printStandings(name):
     new_name = name.replace(" ", "_")
-    cursor.execute("SELECT * FROM " + new_name + " ORDER BY _rank_ ASC;")
+    cursor.execute("SELECT * FROM " + new_name + " ORDER BY _rank_ DESC;")
     result = cursor.fetchall()
     return result
+
+def updateRank(name1, name2, scored, lost, db):
+    new_db = db.replace(" ", "_")
+    cursor.execute("SELECT _rank_ FROM " + new_db + " WHERE _user_ = '" + name1 + "';")
+    player = cursor.fetchone()
+    player1 = player[0]
+    player1 = int(player1)
+    cursor.execute("SELECT _rank_ FROM " + new_db + " WHERE _user_ = '" + name2 + "';")
+    player = cursor.fetchone()
+    player2 = player[0]
+    player2 = int(player2)
+
+    scored = int(scored)
+    lost = int(lost)
+
+    # Testing points
+    print("Winners rank before change")
+    print(player1)
+    print("Losers rank before change")
+    print(player2)
+    # Testing points
+
+    k = 100
+    HGA = 2
+    s = 40
+    aOutcome = scored / (scored + lost)
+    eOutcome = 1 / (1 + 10**((player2 - player1 - HGA) / s))
+
+    new_rank1 = player1 + abs((k * (aOutcome - eOutcome)))
+    new_rank2 = player2 - abs((k * (aOutcome - eOutcome)))
+
+    new_rank1 = round(new_rank1)
+    new_rank2 = round(new_rank2)
+
+    # Testing points
+    print("Winner's gained rank after change")
+    print(new_rank1 - player1)
+    print("Loser's lost rank after change")
+    print(new_rank2 - player2)
+    print("------------------------------------------------")
+    # Testing points
+
+    cursor.execute("UPDATE " + new_db + " SET _rank_ = '" + str(new_rank1) + "' WHERE _user_ = '" + name1 + "';")
+
+    cursor.execute("UPDATE " + new_db + " SET _rank_ = '" + str(new_rank2) + "' WHERE _user_ = '" + name2 + "';")
+    cnx.commit()
+
+    return name1 + " beat " + name2 + " and won " + str(new_rank1 - player1) + " points"
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Creators: Nathan Bonetto and True Sarmiento
-# Date last edited: 5/21/21
+# Date last edited: 5/28/21
 # This is a discord bot that manages a server's elo system
 # Functions:
-# - Have people register within the bot
+# - Have an admin register people in the bot
 # - Have it know Person A vs Person B is fighting
 # - Have a moderator role declare a winner
 # - Give/take elo relative to the elo difference between the two players
@@ -11,24 +11,27 @@
 # - Create a channel displaying player's elo in descending order
 # - Update said channel everytime a player's elo is updated
 # - Every two weeks take away X amount of elo if someone's name has not been used within the bot
-# - Allows players to unregister from the bot
+# - Allow moderators to unregister people from the bot
 
 import discord
-import os
 
-import null as null
 from discord.ext import commands
 from db import create_table
 from db import addMember
 from db import removeMember
 from db import printStandings
+from db import updateRank
 
 DISCORD_TOKEN = ""
 bot = commands.Bot(command_prefix="$")
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print("------------------------------------")
+    print("Bot Name: " + bot.user.name)
+    print("Bot ID: " + str(bot.user.id))
+    print("Discord Version: " + str(discord.__version__))
+    print("------------------------------------")
 
 @bot.command()
 async def setup(ctx):
@@ -39,37 +42,74 @@ async def setup(ctx):
           x = 1
     guild = ctx.guild
     if x == 0:
+      create_table(guild.name)
       # comment out create_role code - True 5/25/21
+      await guild.create_role(name="Bot Moderator")
       await guild.create_role(name="Beginner")
       await guild.create_role(name="Intermidiate", colour=discord.Colour(0x001BFF))
       await guild.create_role(name="Advanced", colour=discord.Colour(0x00FF1B))
       await guild.create_role(name="Master", colour=discord.Colour(0xFF0000))
       await guild.create_role(name="Grandmaster", colour=discord.Colour(0xF7FF00))
+      role = discord.utils.get(ctx.guild.roles, name="Simple Elo Bot")
+      role2 = discord.utils.get(ctx.guild.roles, name="Bot Moderator")
+
       await guild.create_text_channel('current-standings')
-      create_table(guild.name)
-      await ctx.channel.send('Channels and roles have been made!')
+      channel = discord.utils.get(ctx.guild.channels, name="current-standings")
+      await channel.set_permissions(role, send_messages=True)
+      await channel.set_permissions(role2, send_messages=True)
+      await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+
+      await guild.create_text_channel('match-history')
+      channel = discord.utils.get(ctx.guild.channels, name="match-history")
+      await channel.set_permissions(role, send_messages=True)
+      await channel.set_permissions(role2, send_messages=True)
+      await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+
+      await guild.create_text_channel('bot-commands')
+      channel = discord.utils.get(ctx.guild.channels, name="bot-commands")
+      await channel.set_permissions(role, send_messages=True, view_channel=True)
+      await channel.set_permissions(role2, send_messages=True, view_channel=True)
+      await channel.set_permissions(ctx.guild.default_role, view_channel=False)
+
+      await channel.send("Please make sure that I have 0 permissions in any channels EXCEPT the ones I created. Also make sure that the (@Bot Moderator) role is given to moderators only!")
     elif x == 1:
-      await ctx.channel.send('This server has already been setup!')
+      channel = discord.utils.get(ctx.guild.channels, name="bot-commands")
+      await channel.send('This server has already been setup!')
 
 @bot.command()
-async def register(ctx):
-    new_name = str(ctx.message.author)
-    reply = addMember(new_name, ctx.guild.name)
+async def register(ctx, name):
+    reply = addMember(name, ctx.guild.name)
     await ctx.channel.send(reply)
 
 @bot.command()
-async def resign(ctx):
-    new_name = str(ctx.message.author)
-    reply = removeMember(new_name, ctx.guild.name)
+async def resign(ctx, name):
+    reply = removeMember(name, ctx.guild.name)
     await ctx.channel.send(reply)
 
 @bot.command()
 async def update(ctx):
     channel = discord.utils.get(ctx.guild.channels, name="current-standings")
-    channel_id = channel.id
     result = printStandings(ctx.guild.name)
+    await channel.purge(limit=100)
     for i in result:
-        for z in i:
-            await ctx.channel.send(z)
+        string = str(i)
+        string = string.replace("(", "").replace("'", "").replace("'", "").replace(",", " ").replace(")", "")
+        await channel.send(string)
+
+@bot.command()
+async def score(ctx, name1, name2, scored, lost):
+    channel = discord.utils.get(ctx.guild.channels, name="match-history")
+    update = updateRank(name1, name2, scored, lost, ctx.guild.name)
+    await ctx.channel.send("Scores have been update!")
+    await channel.send(update)
+
+    # Updates the server's standings after updating
+    channel = discord.utils.get(ctx.guild.channels, name="current-standings")
+    result = printStandings(ctx.guild.name)
+    await channel.purge(limit=100)
+    for i in result:
+        string = str(i)
+        string = string.replace("(", "").replace("'", "").replace("'", "").replace(",", " ").replace(")", "")
+        await channel.send(string)
 
 bot.run(DISCORD_TOKEN)
