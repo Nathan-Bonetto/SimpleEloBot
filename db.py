a2cb8cb2cb6b81eb62ba9b1d739ee0db3fc34392
Nathan-Bonetto <83558915+Nathan-Bonetto@users.noreply.github.com>
2021-12-25 22:15:48 -0800

    Major Changes
    Added:
    - Rank decay
    - Help command
    - TearDown command removes everything
    - Updates standings every time the database is updated
    - Only admins with Bot Moderator role can use the bot
    - Made some commands easier to use/understand
    - Fix database connection issue
    - Fixed minor issues in the setup command
    - Cleaned up messages the bot sends to look nice
    - Added Dates to the database to use for rank decay
    - Changed the formula for giving/taking away rank (not done yet, but it works for most scoring systems)
    - Does not let player's go below 0 rank by direct loss or decay (would cause issues with the formula)
    -

--- db.py
+++ db.py
@@ -1,19 +1,41 @@
 import mysql.connector
 
+from datetime import datetime, timedelta
+
 DB = ""
 HOST = ""
 PASSWORD = ""
 USER = ""
 
-cnx = mysql.connector.connect(user=USER, password=PASSWORD, host=HOST, database=DB)
-cursor = cnx.cursor()
+cnx = None
+
+def init_db():
+    return mysql.connector.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        database=DB)
+
+def get_cursor(cnx):
+    try:
+        cnx.ping(reconnect=True, attempts=3, delay=5)
+    except mysql.connector.Error as err:
+        # reconnect cursor
+        cnx = init_db()
+        print("Reconnected to database")
+    return cnx.cursor()
+
+cnx = init_db()
+cursor = get_cursor(cnx)
 
 def create_table(name):
+    cursor = get_cursor(cnx)
     new_name = name.replace(" ", "_")
     create = (
         "CREATE TABLE IF NOT EXISTS " + new_name + " ("
         " _user_ VARCHAR(20),"
         " _rank_ SMALLINT,"
+        " _date_ DATE,"
         " PRIMARY KEY (_user_)"
         ");"
     )
@@ -21,31 +43,36 @@ def create_table(name):
     cnx.commit()
 
 def addMember(name, db):
+    cursor = get_cursor(cnx)
     new_db = db.replace(" ", "_")
     try:
-        populate = "INSERT INTO " + new_db + " (_user_, _rank_) VALUES ('" + name + "', 500);"
+        populate = "INSERT INTO " + new_db + " (_user_, _rank_, _date_) VALUES ('" + name + "', 500, '" + datetime.now().strftime("%Y-%m-%d") + "');"
         cursor.execute(populate)
         cnx.commit()
         return "You have successfully been registered! Your rank will now be tracked."
     except:
-        return "You are already registered!"
+        reply = "There was an error trying to register " + str(name) + ", please make sure the name has no spaces and uses only letters and numbers"
+        return reply
 
 
 def removeMember(name, db):
+    cursor = get_cursor(cnx)
     new_db = db.replace(" ", "_")
     delete = "DELETE FROM " + new_db + " WHERE _user_ = '" + name + "';"
     outcome = cursor.execute(delete)
     cnx.commit()
     if outcome == None:
-        return "You have successfully resigned!"
+        return "You have successfully resigned **" + name + "**!"
 
 def printStandings(name):
+    cursor = get_cursor(cnx)
     new_name = name.replace(" ", "_")
-    cursor.execute("SELECT * FROM " + new_name + " ORDER BY _rank_ DESC;")
+    cursor.execute("SELECT _user_, _rank_ FROM " + new_name + " ORDER BY _rank_ DESC;")
     result = cursor.fetchall()
     return result
 
-def updateRank(name1, name2, scored, lost, db):
+def updateRank(name1, scored, name2, lost, db):
+    cursor = get_cursor(cnx)
     new_db = db.replace(" ", "_")
     cursor.execute("SELECT _rank_ FROM " + new_db + " WHERE _user_ = '" + name1 + "';")
     player = cursor.fetchone()
@@ -66,14 +93,16 @@ def updateRank(name1, name2, scored, lost, db):
     print(player2)
     # Testing points
 
+    # Could use an Actual Outcome value for a Best Of X series
     k = 100
-    HGA = 2
-    s = 40
-    aOutcome = scored / (scored + lost)
-    eOutcome = 1 / (1 + 10**((player2 - player1 - HGA) / s))
+    s = 400
+    eOutcomeW = 1 / (1 + 10**((player2 - player1) / s))
+    eOutcomeL = 1 / (1 + 10**((player1 - player2) / s))
 
-    new_rank1 = player1 + abs((k * (aOutcome - eOutcome)))
-    new_rank2 = player2 - abs((k * (aOutcome - eOutcome)))
+
+
+    new_rank1 = player1 + abs((k * (1 - eOutcomeW)))
+    new_rank2 = player2 + abs((k * (0 - eOutcomeL)))
 
     new_rank1 = round(new_rank1)
     new_rank2 = round(new_rank2)
@@ -86,9 +115,77 @@ def updateRank(name1, name2, scored, lost, db):
     print("------------------------------------------------")
     # Testing points
 
-    cursor.execute("UPDATE " + new_db + " SET _rank_ = '" + str(new_rank1) + "' WHERE _user_ = '" + name1 + "';")
+    if new_rank1 <= 0:
+        cursor.execute("UPDATE " + new_db + " SET _rank_ = '" + str(0) + "' WHERE _user_ = '" + name1 + "';")
+    else:
+        cursor.execute("UPDATE " + new_db + " SET _rank_ = '" + str(new_rank1) + "' WHERE _user_ = '" + name1 + "';")
 
-    cursor.execute("UPDATE " + new_db + " SET _rank_ = '" + str(new_rank2) + "' WHERE _user_ = '" + name2 + "';")
+    if new_rank2 <= 0:
+        cursor.execute("UPDATE " + new_db + " SET _rank_ = '" + str(0) + "' WHERE _user_ = '" + name2 + "';")
+    else:
+        cursor.execute("UPDATE " + new_db + " SET _rank_ = '" + str(new_rank2) + "' WHERE _user_ = '" + name2 + "';")
+
+    cursor.execute("UPDATE " + new_db + " SET _date_ = '" + datetime.now().strftime("%Y-%m-%d") + "' WHERE _user_ = '" + name1 + "';")
+
+    cursor.execute("UPDATE " + new_db + " SET _date_ = '" + datetime.now().strftime("%Y-%m-%d") + "' WHERE _user_ = '" + name2 + "';")
+
     cnx.commit()
 
-    return name1 + " beat " + name2 + " and won " + str(new_rank1 - player1) + " points"
+    return "**" + name1 + "**" + " beat " +  "**" + name2 + "** " + "``" +  str(scored) + "-" +  str(lost) + "``" +  " and won " + str(new_rank1 - player1) + " points"
+
+def decay():
+    cursor = get_cursor(cnx)
+    cursor.execute("SHOW TABLES;")
+    servers = cursor.fetchall()
+    servers = str(servers)
+    servers = servers.replace("[", "").replace("(", "").replace("'", "").replace(",", "").replace(")", "").replace("]", "")
+    array = []
+
+    while servers.find(" ") != -1:
+        serverName = servers[0 : servers.find(" ")]
+        array.append(serverName)
+        servers = servers[servers.find(" ") + 1 : len(servers)]
+
+    serverName = servers
+    array.append(serverName)
+
+    for i in array:
+        dt = datetime.now()
+        td = timedelta(days=14)
+        dt = dt - td
+        dt = dt.strftime("%Y-%m-%d")
+        cursor.execute("SELECT _user_ FROM " + str(i) + " WHERE _date_ < '" + dt + "';")
+        decayNames = cursor.fetchall()
+        if len(decayNames) > 0:
+            decayNames = str(decayNames)
+            decayNames = decayNames.replace("[", "").replace("(", "").replace("'", "").replace(",", "").replace(")", "").replace("]", "")
+            decayNamesArray = []
+            while decayNames.find(" ") != -1:
+                name = decayNames[0 : decayNames.find(" ")]
+                decayNamesArray.append(name)
+                decayNames = decayNames[decayNames.find(" ") + 1: len(decayNames)]
+
+            name = decayNames
+            decayNamesArray.append(name)
+
+            for j in decayNamesArray:
+                cursor.execute("SELECT _rank_ FROM " + str(i) + " WHERE _user_ = '" + str(j) + "';")
+                check = cursor.fetchall()
+                check = str(check)
+                check = check.replace("[", "").replace("(", "").replace("'", "").replace(",", "").replace(")","").replace("]", "")
+                check = int(check)
+
+                if check - 20 <= 0:
+                    cursor.execute("UPDATE " + str(i) + " SET _rank_ = " + str(0) + " WHERE _user_ = '" + str(j) + "';")
+                else:
+                    cursor.execute("UPDATE " + str(i) + " SET _rank_ = _rank_ - " + str(20) + " WHERE _user_ = '" + str(j) + "';")
+
+    cnx.commit()
+
+
+
+def delete_table(table_name):
+    cursor = get_cursor(cnx)
+    new_table_name = table_name.replace(" ", "_")
+    cursor.execute("DROP TABLE " + new_table_name + ";")
+    cnx.commit()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Creators: Nathan Bonetto and True Sarmiento
-# Date last edited: 5/28/21
+# Date last edited: 12/25/21
 # This is a discord bot that manages a server's elo system
 # Functions:
 # - Have an admin register people in the bot
@@ -7,13 +7,14 @@
 # - Have a moderator role declare a winner
 # - Give/take elo relative to the elo difference between the two players
 # - Store the players elo
-# - Give roles based on a player's elo "bracket"
 # - Create a channel displaying player's elo in descending order
 # - Update said channel everytime a player's elo is updated
 # - Every two weeks take away X amount of elo if someone's name has not been used within the bot
 # - Allow moderators to unregister people from the bot
 
 import discord
+import threading
+import asyncio
 
 from discord.ext import commands
 from db import create_table
@@ -21,9 +22,18 @@ from db import addMember
 from db import removeMember
 from db import printStandings
 from db import updateRank
+from db import decay
+from db import delete_table
 
-DISCORD_TOKEN = ""
-bot = commands.Bot(command_prefix="$")
+DISCORD_TOKEN = "ODQ1MzgwNDQ1MzE0NzQ0MzIx.YKgH5A.gSLy52Qyo_aZRNs2_5wsPm1qL8k"
+bot = commands.Bot(command_prefix="$", help_command=None)
+
+def decayTimer():
+  threading.Timer((3600 * 24), decayTimer).start()
+  decay()
+
+decayTimer()
+
 
 @bot.event
 async def on_ready():
@@ -33,83 +43,150 @@ async def on_ready():
     print("Discord Version: " + str(discord.__version__))
     print("------------------------------------")
 
+async def check_roles(ctx, message):
+    for i in message.author.roles:
+        if i == discord.utils.get(ctx.guild.roles, name="Bot Moderator"):
+            return True
+    return False
+
+@bot.command()
+async def help(ctx):
+    if ctx.author.guild_permissions.administrator:
+        await ctx.channel.send("```"
+            "help     - Shows this message\n"
+            "register - this followed by a name registers a name (no spaces, max 20 characters)\n" 
+            "resign   - this followed by a player's name removes the player and deletes their data\n"   
+            "score    - to submit a score, MUST BE SUBMITED IN THIS ORDER(no commas): winner's name, winner's score, loser's name, loser's score\n"    
+            "setup    - Setsup the server with channels and roles (needs to be done once)\n"    
+            "tearDown - Deletes everything, nothing is recoverable once this command is executed\n" 
+            "update   - Updates the current-standings channel manually"
+            "```")
+
+
 @bot.command()
 async def setup(ctx):
-    x = 0
-    for guild in bot.guilds:
-      for channel in guild.text_channels:
-        if channel.name == 'current-standings':
-          x = 1
-    guild = ctx.guild
-    if x == 0:
-      create_table(guild.name)
-      # comment out create_role code - True 5/25/21
-      await guild.create_role(name="Bot Moderator")
-      await guild.create_role(name="Beginner")
-      await guild.create_role(name="Intermidiate", colour=discord.Colour(0x001BFF))
-      await guild.create_role(name="Advanced", colour=discord.Colour(0x00FF1B))
-      await guild.create_role(name="Master", colour=discord.Colour(0xFF0000))
-      await guild.create_role(name="Grandmaster", colour=discord.Colour(0xF7FF00))
-      role = discord.utils.get(ctx.guild.roles, name="Simple Elo Bot")
-      role2 = discord.utils.get(ctx.guild.roles, name="Bot Moderator")
+    if ctx.author.guild_permissions.administrator:
+        x = 0
+        for channel in ctx.guild.text_channels:
+            if channel.name == 'current-standings':
+              x = 1
+        guild = ctx.guild
+        if x == 0:
+          create_table(guild.name)
+          await guild.create_role(name="Bot Moderator")
+          await guild.create_role(name="Grandmaster", colour=discord.Colour(0xF7FF00), hoist=True)
+          await guild.create_role(name="Master", colour=discord.Colour(0xFF0000), hoist=True)
+          await guild.create_role(name="Advanced", colour=discord.Colour(0x00FF1B), hoist=True)
+          await guild.create_role(name="Intermediate", colour=discord.Colour(0x001BFF), hoist=True)
+          await guild.create_role(name="Beginner", hoist=True)
+          role = discord.utils.get(ctx.guild.roles, name="Simple Elo Bot")
+          role2 = discord.utils.get(ctx.guild.roles, name="Bot Moderator")
 
-      await guild.create_text_channel('current-standings')
-      channel = discord.utils.get(ctx.guild.channels, name="current-standings")
-      await channel.set_permissions(role, send_messages=True)
-      await channel.set_permissions(role2, send_messages=True)
-      await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+          await guild.create_text_channel('current-standings')
+          channel = discord.utils.get(ctx.guild.channels, name="current-standings")
+          await channel.set_permissions(role, send_messages=True)
+          await channel.set_permissions(role2, send_messages=True)
+          await channel.set_permissions(ctx.guild.default_role, send_messages=False)
 
-      await guild.create_text_channel('match-history')
-      channel = discord.utils.get(ctx.guild.channels, name="match-history")
-      await channel.set_permissions(role, send_messages=True)
-      await channel.set_permissions(role2, send_messages=True)
-      await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+          await guild.create_text_channel('match-history')
+          channel = discord.utils.get(ctx.guild.channels, name="match-history")
+          await channel.set_permissions(role, send_messages=True)
+          await channel.set_permissions(role2, send_messages=True)
+          await channel.set_permissions(ctx.guild.default_role, send_messages=False)
 
-      await guild.create_text_channel('bot-commands')
-      channel = discord.utils.get(ctx.guild.channels, name="bot-commands")
-      await channel.set_permissions(role, send_messages=True, view_channel=True)
-      await channel.set_permissions(role2, send_messages=True, view_channel=True)
-      await channel.set_permissions(ctx.guild.default_role, view_channel=False)
+          await guild.create_text_channel('bot-commands')
+          channel = discord.utils.get(ctx.guild.channels, name="bot-commands")
+          await channel.set_permissions(role, send_messages=True, view_channel=True)
+          await channel.set_permissions(role2, send_messages=True, view_channel=True)
+          await channel.set_permissions(ctx.guild.default_role, view_channel=False)
 
-      await channel.send("Please make sure that I have 0 permissions in any channels EXCEPT the ones I created. Also make sure that the (@Bot Moderator) role is given to moderators only!")
-    elif x == 1:
-      channel = discord.utils.get(ctx.guild.channels, name="bot-commands")
-      await channel.send('This server has already been setup!')
+          await channel.send("Please make sure that I have 0 permissions in any channels EXCEPT the ones I created. Only people with admin privileges AND the  "+ role2.mention + " will be able to use my commands!")
+
+        elif x == 1:
+          channel = discord.utils.get(ctx.guild.channels, name="bot-commands")
+          await channel.send('This server has already been setup!')
+
+@bot.command()
+async def tearDown(ctx):
+    def check(msg):
+        return msg.content == "Yes"
+    if await check_roles(ctx, ctx.message) and ctx.author.guild_permissions.administrator:
+        await ctx.channel.send("Are you sure? This command means I delete everything I made inside this server including the database, there is no "
+                               "recovering information once I delete it. Would you still like me to delete everything? "
+                               "(type \"Yes\" exactly for me to do it, or wait 5 seconds for the request to expire)")
+        try:
+            msg = await bot.wait_for("message", check=check, timeout=5)
+        except asyncio.TimeoutError:
+            await ctx.channel.send("Request has timed out, your information is still here!")
+        if msg:
+            role = discord.utils.get(ctx.guild.roles, name="Bot Moderator")
+            await role.delete()
+            role = discord.utils.get(ctx.guild.roles, name="Beginner")
+            await role.delete()
+            role = discord.utils.get(ctx.guild.roles, name="Intermediate")
+            await role.delete()
+            role = discord.utils.get(ctx.guild.roles, name="Advanced")
+            await role.delete()
+            role = discord.utils.get(ctx.guild.roles, name="Master")
+            await role.delete()
+            role = discord.utils.get(ctx.guild.roles, name="Grandmaster")
+            await role.delete()
+
+
+            channel = discord.utils.get(ctx.guild.channels, name="current-standings")
+            await channel.delete()
+
+            channel = discord.utils.get(ctx.guild.channels, name="match-history")
+            await channel.delete()
+
+            channel = discord.utils.get(ctx.guild.channels, name="bot-commands")
+            await channel.delete()
+
+            delete_table(ctx.guild.name)
 
 @bot.command()
 async def register(ctx, name):
-    reply = addMember(name, ctx.guild.name)
-    await ctx.channel.send(reply)
+    if await check_roles(ctx, ctx.message) and ctx.author.guild_permissions.administrator:
+        reply = addMember(name, ctx.guild.name)
+        await ctx.channel.send(reply)
+        await update(ctx)
 
 @bot.command()
 async def resign(ctx, name):
-    reply = removeMember(name, ctx.guild.name)
-    await ctx.channel.send(reply)
+    if await check_roles(ctx, ctx.message) and ctx.author.guild_permissions.administrator:
+        reply = removeMember(name, ctx.guild.name)
+        await ctx.channel.send(reply)
+        await update(ctx)
 
 @bot.command()
 async def update(ctx):
-    channel = discord.utils.get(ctx.guild.channels, name="current-standings")
-    result = printStandings(ctx.guild.name)
-    await channel.purge(limit=100)
-    for i in result:
-        string = str(i)
-        string = string.replace("(", "").replace("'", "").replace("'", "").replace(",", " ").replace(")", "")
-        await channel.send(string)
+    if await check_roles(ctx, ctx.message) and ctx.author.guild_permissions.administrator:
+        channel = discord.utils.get(ctx.guild.channels, name="current-standings")
+        result = printStandings(ctx.guild.name)
+        await channel.purge(limit=1000)
+        await channel.send("```Name                 Rank```")
+        for i in result:
+            string = str(i)
+            string = string.replace("(", "").replace("'", "").replace("'", "").replace(",", "").replace(")", "")
+
+            temp = string.find(" ")
+            name = string[0 : temp]
+            space1 = ""
+            for j in range(40 - len(name)):
+                space1 +=  " "
+
+            string = string[temp + 1 : len(string)]
+            rank = string[0 : temp]
+
+            await channel.send(" " + name + space1 + rank)
 
 @bot.command()
-async def score(ctx, name1, name2, scored, lost):
-    channel = discord.utils.get(ctx.guild.channels, name="match-history")
-    update = updateRank(name1, name2, scored, lost, ctx.guild.name)
-    await ctx.channel.send("Scores have been update!")
-    await channel.send(update)
-
-    # Updates the server's standings after updating
-    channel = discord.utils.get(ctx.guild.channels, name="current-standings")
-    result = printStandings(ctx.guild.name)
-    await channel.purge(limit=100)
-    for i in result:
-        string = str(i)
-        string = string.replace("(", "").replace("'", "").replace("'", "").replace(",", " ").replace(")", "")
-        await channel.send(string)
+async def score(ctx, name1, scored, name2, lost):
+    if await check_roles(ctx, ctx.message) and ctx.author.guild_permissions.administrator:
+        channel = discord.utils.get(ctx.guild.channels, name="match-history")
+        rankUpdate = updateRank(name1, scored, name2, lost, ctx.guild.name)
+        await ctx.channel.send("Scores have been update!")
+        await channel.send(rankUpdate)
+        await update(ctx)
 
 bot.run(DISCORD_TOKEN)
